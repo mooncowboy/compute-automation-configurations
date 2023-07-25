d408e962b03b94eca72df8966c8ef00e7bbdc8de
Ricardo Fiel <rifiel@microsoft.com>
2023-07-25 20:29:58 +0100

    msg2 for vmss

--- automate-iis.ps1
+++ automate-iis.ps1
@@ -23,4 +23,4 @@
 # SOFTWARE.
 
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World updated from host $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World updated again from host $($env:computername) !"
